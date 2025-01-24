96733a6e2f33dad289574309b16db050d7a82dff
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2025-01-24 14:49:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+function manipulateData() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([1, 2, 3, 4]);
+        }, 3000);
+    })
+    .then((array) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const evenNumbers = array.filter(num => num % 2 === 0);
+                document.getElementById('output').innerText = evenNumbers.join(', ');
+                resolve(evenNumbers);
+            }, 1000);
+        });
+    })
+    .then((evenNumbers) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const multipliedNumbers = evenNumbers.map(num => num * 2);
+                document.getElementById('output').innerText = multipliedNumbers.join(', ');
+                resolve(multipliedNumbers);
+            }, 2000);
+        });
+    });
+}
+
+manipulateData();
+
